821aa3cf96afc21f601f541c2c086679c9fad455
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-08-05 16:25:17 -0300

    revert the update
    Updated formatAll function to exclude additional slow notations and modified notation handling for SimplifiedWritten and ColoredDominoes.

--- testing/preset_testing_worker.js
+++ testing/preset_testing_worker.js
@@ -9,7 +9,7 @@ onmessage = function(e) {
 function formatAll(num, exclude_slow = false) {
     for (k in EternalNotations.HTMLPresets) {
         // Fast-Growing Hierarchy, Polygonal, and Prestige Layer notations run a little too slowly for my tastes. Excluding them should make this run much faster.
-        if (exclude_slow && (k == "FastGrowingHierarchy" || k == "HardyHierarchy")) continue;
+        if (exclude_slow && (k == "FastGrowingHierarchy" || k == "HardyHierarchy" || k == "Triangular" || k == "Square" || k == "OmegaLayers" || k == "OmegaLayersRamped" || k == "OmegaMetaZeroAlphaAmount")) continue;
         formatOne(k, num);
     }
     EternalNotations.physicalScale(num);
@@ -24,7 +24,16 @@ function formatOne(key, num) {
     catch { return; }
     if (notation === undefined) return;
     if (typeof notation == "function") {
-        if (key == "Polynomial" || key == "RationalFunction") {
+        if (key == "SimplifiedWritten") {
+            notation = notation(10);
+        }
+        else if (key == "ColoredDominoes") {
+            for (let i = 6; i < 21; i += 3) {
+                notation(i).format(num);
+            }
+            return;
+        }
+        else if (key == "Polynomial" || key == "RationalFunction") {
             notation(10).format(num);
             notation(2).format(num);
             notation(3).format(num);
